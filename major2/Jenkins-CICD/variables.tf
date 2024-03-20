ba9b92c885cdfaf3331e9b5b97b1c741b27258bc
UrvashiRathore <89251583+UrvashiRathore@users.noreply.github.com>
2024-03-20 19:05:27 +0530

    Update variables.tf

--- major2/Jenkins-CICD/variables.tf
+++ major2/Jenkins-CICD/variables.tf
@@ -4,26 +4,26 @@ variable "aws_region" {
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "purplehaze"
+  default     = "MJ2"
 }
 variable "instance_type" {
   description = "instance type for ec2"
-  default     = "t2.medium"
+  default     = "t2.large"
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0df4b2961410d4cff"
+  default     = "ami-09c8d5d747253fb7a"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "purplehaze777"
+  default     = "major2.1"
 }
 
 variable "dynamodb_table" {
   description = "The name of the dynamodb table"
   type        = string
-  default     = "purplehaze777-dynamodb-table"
+  default     = "majorProj"
 }
 
 variable "acl" {
